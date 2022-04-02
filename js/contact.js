4bd0f18e23390bd1cafe35ddf017b830d88960a3
Elizabeth <elisha.elizabeth@gmail.com>
2022-04-02 13:04:05 -0700

    updates

--- js/contact.js
+++ js/contact.js
@@ -1,6 +1,11 @@
-var msg = 'Sign up to receive special offers!'
-function updateMessage(){
-    var el = document.getElementById('message')
-    el.textContent = msg 
+let question = prompt ('How many bottles do you want to order?');
+console.log('question',);
+
+if (question >= 5) {
+     alert('Great! We hope you will enjoy!')
+} else if (question < 5) {
+    alert ('Did you know you can receive 20% off for 5+ bottles?')
+} else {
+     alert('We apologize, please call 888-123-4567')
+     console.log ('Thats too bad')
 }
-updateMessage();
