20dd0ab16de262ceac236d600a9b95e0affa6c27
Elizabeth <elisha.elizabeth@gmail.com>
2022-04-02 12:42:01 -0700

    js updates

--- js/contact.js
+++ js/contact.js
@@ -1,10 +1,6 @@
-var attempts = 2;
-while (attempts > 0) {
-    var answer = prompt('What is our best seller? /n/ Answer: Pinot Noir');
-    if (answer = "Pinor Noir") {
-        document.write('You are correct!');
-        break;
-    } 
-if (attempts == 0) {
-    alert('That is not our best seller');
-}}
+var msg = 'Sign up to receive special offers!'
+function updateMessage(){
+    var el = document.getElementById('message')
+    el.textContent = msg 
+}
+updateMessage();
