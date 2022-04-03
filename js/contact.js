3501c1da2337d78989d009866bef5ff485cefb55
Elizabeth <elisha.elizabeth@gmail.com>
2022-04-03 11:04:49 -0700

    fixes

--- js/contact.js
+++ js/contact.js
@@ -1,18 +1,24 @@
 function winegame(){
-    let userAnswer = prompt('Guess how many wine bottles we have\r\ Guess 500');
+    let userAnswer = prompt('Guess how many wine bottles we have\r\Tip: Guess 500');
     let correctAnswer = 500;
     let attempts = 5;
 
-    for (let i = 0; i < attempts; i++)
-        while (userAnswer < 1 || userAnswer > 100) {
+    for (let i = 1; i < attempts; i++){
+        while (userAnswer < 1 || userAnswer > 500){
             userAnswer = prompt('Please Try Again!');
         }   
-        if (userAnswer == 500){
-            alert ('Correct! Use code 10OFF at checkout to receive 10% off!');
-        } else if (userAnswer != 500) {
-            alert ('Sorry, that is incorrect');
+        if (userAnswer == correctAnswer){
+            alert('Correct! Use code 10OFF at checkout to receive 10% off!');
+            break;
+        } else if (userAnswer > correctAnswer){
+            userAnswer = prompt('Sorry that is too high, please try again!');      
+        } else if (userAnswer < correctAnswer){
+            userAnswer = prompt('Sorry, that is too low, please try again!');
         }
-}
+        if(i = 7){
+                alert('The correct answer was ' + correctAnswer);
+        }
+    }
 
 // let question = prompt ('How many bottles do you want to order?');
 // console.log('question',);
